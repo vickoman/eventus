f5c2912dee42df4dec739df6ece10f77a6f42a55
Victor Manuel <vicmandlagasca@gmail.com>
2014-10-23 22:31:37 -0500

    SETUP EVETUS clone de welcu

---
+++ eventus/settings/base.py
@@ -0,0 +1,67 @@
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '-*)+(bo1qckeecgvcdfe^@a_*x3o1%mrfhrxae)f1-+mw^ai+w'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+DJANGO_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+    )
+
+
+#Aplicaciones locales
+LOCAL_APPS = (
+        'apps.events',        
+    )
+
+
+#Aplicaciones de terceros
+THIRD_PARTY_APPS = (        
+    )
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'eventus.urls'
+
+WSGI_APPLICATION = 'eventus.wsgi.application'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
---
+++ eventus/settings/local.py
@@ -0,0 +1,27 @@
+from .base import *
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+#POSTGRES
+DATABASES = {
+	'default' : {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'eventus',
+		'USER': 'postgres',
+		'PASSWORD': 'Mm0925163347',
+		'HOST': 'localhost',
+		'PORT': '5432',
+	}
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_URL = '/static/'
